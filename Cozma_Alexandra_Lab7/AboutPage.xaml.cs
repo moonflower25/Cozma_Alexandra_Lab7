49c763cbc9d40746eb621eee46778fe107ab2d33
Alexandra Cozma <ale.cozma2526@gmail.com>
2022-11-29 11:38:21 +0200

    Add project files.

---
+++ Cozma_Alexandra_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Cozma_Alexandra_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Cozma_Alexandra_Lab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Cozma_Alexandra_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
